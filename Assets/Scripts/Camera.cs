a6a431fc19a60bb9e7757af6abce8786e85a0f79
GoldKenway <59236576+GoldKenway@users.noreply.github.com>
2020-10-20 10:03:30 -0500

    added the camer, locked to y position

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GetComponent<UnityEngine.Camera>().main.transform.position.y = 0.62f;
+        float x = this.transform.position.x;
+        float z = this.transform.position.z;
+        float y = 0;
+        Vector3 newpostion = new Vector3(x, y, z); 
+        this.transform.position = newpostion;
+    }
+}
