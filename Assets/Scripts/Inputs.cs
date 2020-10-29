fa4e3a1d130fd7f4960edf24208b929b191417b4
Qwertybanana <kinsellat7052@my.uwstout.edu>
2020-10-28 21:38:30 -0500

    Added new sprite sheet, created new animations, worked with animator
    Added the new sprite sheet, with idle, hit, dead, and special animations, created said animations. Applied Idle animation to main character, so it plays when not punching.
    
    Connected PlayerMovement script to player.
    
    Refined animations for punches. Punch 2 and 3 work better, but first punch gets stuck.

---
+++ Assets/Scripts/Inputs.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inputs : MonoBehaviour
+{
+    [SerializeField] private Mov Mov;
+    float HorizontalMovement; 
+    float VerticalMovement;
+    
+    // Start is called before the first frame update
+   public void Awake()
+    {
+        Mov = GetComponent<Mov>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalMovement = Input.GetAxis("Horizontal");
+        VerticalMovement = Input.GetAxis("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        Mov.Move(HorizontalMovement, VerticalMovement, false);
+    }
+}
--- Assets/Scripts/Mov.cs
+++ Assets/Scripts/Mov.cs
@@ -7,9 +7,12 @@ using UnityEngine;
 public class Mov : MonoBehaviour
 {
 
+    //public Animator animator;
+
     public Animator animator;
     private Rigidbody2D _rigidbody;
     public Transform PunchAttackPoint;
+
 
     [SerializeField] private float hspeed = 10f;
     [SerializeField] private float vspeed = 6f;
@@ -24,99 +27,55 @@ public class Mov : MonoBehaviour
     public float movementSpeed = 3;
     public float attackRange = 0.5f;
     public float attackRate = 2f;
-    
+
     public LayerMask enemyLayers;
     public int hit = 1;
     float nextAttackTime = 0f;
 
+    public int hitCount = 0;
+    float lastAttackTime = 0;
+    public float maxComboDelay = 0.9f;
 
-    private void Awake() {
+    private void Awake()
+    {
 
-        rb2D = GetComponent<Rigidbody2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
 
-    public void Move(float hMove, float vMove, bool jump) {
+    public void Move(float hMove, float vMove, bool jump)
+    {
 
         if (canMove)
         {
             Vector3 targetVelocity = new Vector2(hMove * hspeed, vMove * vspeed);
+
+
+            _rigidbody.velocity = Vector3.SmoothDamp(_rigidbody.velocity, targetVelocity, ref velocity, movementSmooth);// takes in some inpute values and sets veloctiy in a smoothed out movement velocity. 
+
+
+            //roate if facing the wrong way
+            if (hMove > 0 && !facingRight)
+            {
+                flip();
+            }
+            else if (hMove < 0 && facingRight)
+            {
+                flip();
+            }
+
         }
-
-        rb2D.velocity = Vector3.SmoothDamp(rb2d.velocity,targetVelocity,ref velocity, movementSmooth);// takes in some inpute values and sets veloctiy in a smoothed out movement velocity. 
-
-        //roate if facing the wrong way
-        if (hMove > 0 && !facingRight)
-        {
-            flip();
-        }
-        else if( hMove < 0 && facingRight)
-        {
-            flip();
-        }
-
-
     }
 
-    private void flip() {
+    private void flip()
+    {
         facingRight = !facingRight;
         transform.Rotate(0, 180, 0);
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("I am alive!");
-        animator = gameObject.GetComponent<Animator>();
-        animator.SetBool("isPunching", false);
-        _rigidbody = GetComponent<Rigidbody2D>();
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * Time.deltaTime * movementSpeed;
-        if (Time.time >= nextAttackTime)
-        {
-            if (Input.GetKeyDown(KeyCode.F))
-            {
-                print("F has been pressed");
-                animator.Play("Punch Animation", -1, 0f);
-                PunchAttack();
-                nextAttackTime = Time.time + 1f / attackRate;
-            }
-        }
-
-
-    
-
-    }
-
-    void PunchAttack()
-    {
-        //attack animation currently plays through Update
-
-        //detect enemies in range of punch
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(PunchAttackPoint.position, attackRange, enemyLayers);
-
-        //damage enemies
-        foreach(Collider2D enemy in hitEnemies)
-        {
-            Debug.Log("We hit " + enemy.name);
-            enemy.GetComponent<EnemyGhost>().TakeDamage(hit);
-        }
-
-    }
-
-
-    //shows punch attack point on screen when editing
-    void OnDrawGizmosSelected()
-    {
-        if (PunchAttackPoint == null)
-            return;
-        Gizmos.DrawWireSphere(PunchAttackPoint.position, attackRange);
-    }
 
 
 }
+
+
+   
