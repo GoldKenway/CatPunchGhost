1f5dcfd37cb91002fdca3b2932db36d18af0f407
GoldKenway <59236576+GoldKenway@users.noreply.github.com>
2020-11-01 18:12:45 -0600

    Created ProgressBar

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ProgressBar : MonoBehaviour
+{
+
+	public Slider slider;
+
+	public void SetProgress(int EnemyKilled)
+	{
+		slider.value = EnemyKilled;
+
+	}
+
+
+
+}
