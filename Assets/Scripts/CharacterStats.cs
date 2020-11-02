60b05a7fc2b45b9296074eaa62d089afca2453c8
GoldKenway <59236576+GoldKenway@users.noreply.github.com>
2020-11-01 21:43:40 -0600

    added character stats as well as vars for setting them
    you can now change the max health of the character to your will. there's a stack overflow error but idk why

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterStats : MonoBehaviour
+{
+
+	public Text Health;
+
+	public Text Charge;
+
+	public Text Lives;
+
+	public int playerHealth = 500;
+
+	public int chargeCount = 0;
+
+	private int lifeCount = 3;
+
+
+	public void GameOver()
+	{
+
+		//plays gameover screen
+
+
+	}
+
+	public void characterDeath(int lives)
+	{
+		//checks if player has no lives 
+
+		if (lives <= 0)
+		{
+			GameOver();
+
+		}
+
+		revive(lives);
+
+	}
+
+	private void revive(int lives)
+	{
+		lives--;
+		//revive the player 
+
+
+	}
+
+	void Update()
+	{
+		Health.text = playerHealth.ToString("0");
+		Charge.text = chargeCount.ToString("0");
+		Lives.text = lifeCount.ToString("0");
+
+	}
+
+}
