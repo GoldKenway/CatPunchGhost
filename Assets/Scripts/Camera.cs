c2315f9c7422b5b4d4393368c11d78579837729d
GoldKenway <59236576+GoldKenway@users.noreply.github.com>
2020-11-12 09:29:54 -0600

    added boss animations

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -5,38 +5,45 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    public Transform target;
-    public float speed = 0.7f;
-    GameObject player;
+	public Transform target;
+	public float speed = 0.7f;
+	GameObject player;
+
+	//change player to a camera point object
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+		player = GameObject.FindWithTag("CameraPoint");
+
+		if (player)
+		{
+			target = player.transform;
+
+		}
+
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//GetComponent<UnityEngine.Camera>().main.transform.position.y = 0.62f;
+
+
+		transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, transform.position.y,
+		  -2.5f), speed * Time.deltaTime);
 
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        /*
-        player = GameObject.FindWithTag("MainCharacter");
 
-        if (player)
-        {
-            target = player.transform;
+	}
 
-        }
-        */
-       
-    }
+	void isPlayerMoving()
+	{
+		// chack if the player is moving, and move the camera accordingly.
 
-    // Update is called once per frame
-    void Update()
-    {
-        //GetComponent<UnityEngine.Camera>().main.transform.position.y = 0.62f;
+	}
 
-        /*
-        transform.position =  Vector3.Lerp(transform.position, new Vector3(target.position.x, transform.position.y,
-          target.position.z), speed * Time.deltaTime);
-        */
-
-    
-
-    }
 }
