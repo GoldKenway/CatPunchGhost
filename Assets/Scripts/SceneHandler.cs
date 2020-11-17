971d71eb77166682d3d55a86684499c195e88328
GoldKenway <59236576+GoldKenway@users.noreply.github.com>
2020-11-16 22:02:51 -0600

    added level2, working with the stuff

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,41 @@
+﻿
+// SceneA.
+// SceneA is given the sceneName which will
+// load SceneB from the Build Settings
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public Animator animator;
+    private int LevelToLoad;
+
+    void Update() 
+    {
+        FadeToLevel(1);
+    }
+
+    void Start()
+    {
+        Debug.Log("LoadLevel1");
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        animator.SetTrigger("FadeOut");
+        
+        Debug.Log("LoadingScenenumber to load: " + levelIndex);
+        //SceneManager.LoadScene(scenename);
+    }
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(LevelToLoad);
+
+
+
+    }
+}
