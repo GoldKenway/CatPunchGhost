9b2e4423ffc8d308c1c5c8e24ca19ddbaa05ed09
Qwertybanana <kinsellat7052@my.uwstout.edu>
2020-12-15 10:27:24 -0600

    Minor animation fixes - player and small ghost no longer get stuck in attack animations

---
+++ Assets/Scripts/EnemyProjectiles.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectiles : MonoBehaviour
+{
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Test firing
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Shoot();
+        }
+
+
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+
+}
